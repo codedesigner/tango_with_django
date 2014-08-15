8d5c989b3b107872f979eadedba8f37f0c55efea
Prasanna Paranjape <prasannaparanjape@gmail.com>
2014-08-15 16:39:06 +0530

    added category view and template with links to pages.

--- tango_with_djnago_project/rango/views.py
+++ tango_with_djnago_project/rango/views.py
@@ -7,7 +7,23 @@ def index(request):
   context = RequestContext(request)
   category_list = Category.objects.order_by('-likes')[:5]
   context_dict = {'categories':category_list}
+  for category in category_list:
+    category.url = category.name.replace(' ', '_')
   return render_to_response('rango/index.html', context_dict, context)
 
 def about(request):
   return render(request, 'rango/about.html')
+
+def category(request, category_name_url):
+  context = RequestContext(request)
+  category_name = category_name_url.replace('_', ' ')
+  context_dict = {'category_name' : category_name}
+  try:
+    category = Category.objects.get(name=category_name)
+    pages = Page.objects.filter(category=category)
+    context_dict['pages'] = pages
+    context_dict['category'] = category
+  except Category.DoesNotExist:
+    pass
+
+  return render_to_response('rango/category.html', context_dict, context)
