1ed0ef54a6615cbe63b50732aa722e13f97867c2
Prasanna Paranjape <prasannaparanjape@gmail.com>
2014-08-17 13:53:04 +0530

    added most viewed link's list to index

--- tango_with_djnago_project/populate_rango.py
+++ tango_with_djnago_project/populate_rango.py
@@ -5,39 +5,47 @@ def populate():
 
   add_page(cat=python_cat,
         title="Official Python Tutorial",
-        url="http://docs.python.org/2/tutorial/")
+        url="http://docs.python.org/2/tutorial/",
+        views=128)
 
   add_page(cat=python_cat,
         title="How to Think like a computer Scientist",
-        url="http://www.greenteapress.com/thinkpython/")
+        url="http://www.greenteapress.com/thinkpython/",
+        views=110)
 
   add_page(cat=python_cat,
         title="Learn Pyhton in 10 minutes",
-        url="http://www.korokithakis.net/tutorials/python/")
+        url="http://www.korokithakis.net/tutorials/python/",
+        views=98)
 
   django_cat = add_cat("Django", views=64, likes=32)
 
   add_page(cat=django_cat,
         title="Official Django Tutorial",
-        url="http://docs.djangoproject.com/en/1.5/intro/tutorial01")
+        url="http://docs.djangoproject.com/en/1.5/intro/tutorial01",
+        views=130)
 
   add_page(cat=django_cat,
         title="Django Rocks",
-        url="http://www.djangorocks.com/")
+        url="http://www.djangorocks.com/",
+        views=44)
 
   add_page(cat=django_cat,
         title="How to Tango with Django",
-        url="http://www.tangowithdjango.com/")
+        url="http://www.tangowithdjango.com/",
+        views=80)
 
   frame_cat = add_cat("Other Frameworks", views=32, likes=16)
 
   add_page(cat=frame_cat,
         title="Bottle",
-        url="http://bottleplay.org/docs/dev/")
+        url="http://bottleplay.org/docs/dev/",
+        views=50)
 
   add_page(cat=frame_cat,
         title="Flask",
-        url="http://flask.pocoo.org")
+        url="http://flask.pocoo.org",
+        views=50)
 
   # Print out what we have added to user.
   for c in Category.objects.all():
@@ -45,11 +53,13 @@ def populate():
       print "- {0} - {1}".format(str(c), str(p))
 
 def add_page(cat, title, url, views=0):
-  p, created = Page.objects.get_or_create(category=cat, title=title, url=url, views=views)
+  p, created = Page.objects.get_or_create(category=cat, title=title, url=url)
+  p.views = views
+  p.save()
   return p
 
 def add_cat(name, views=0, likes=0):
-  c, created = Category.objects.get_or_create(name=name)#, views=views, likes=likes)
+  c, created = Category.objects.get_or_create(name=name)
   print Category.objects.get_or_create(name=name)
   print c
   print created
--- tango_with_djnago_project/rango/views.py
+++ tango_with_djnago_project/rango/views.py
@@ -5,8 +5,9 @@ from rango.models import Category, Page
 
 def index(request):
   context = RequestContext(request)
-  category_list = Category.objects.order_by('-likes')[:5]
-  context_dict = {'categories':category_list}
+  category_list = Category.objects.order_by('-likes')[:5]  
+  page_list = Page.objects.order_by('-views')[:5]
+  context_dict = {'categories':category_list, 'pages':page_list}
   for category in category_list:
     category.url = category.name.replace(' ', '_')
   return render_to_response('rango/index.html', context_dict, context)
